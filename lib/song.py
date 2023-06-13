9391aee7d7b5c048bd0e34de62f81420e58710de
florence wangechi <florencewangechim@gmail.com>
2023-06-13 12:19:22 +0300

    Classes

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,21 @@
 from config import CONN, CURSOR
+import sqlite3
 
 class Song:
-    pass
+    
+    def __init__(self,name,album):
+        self.name=name
+        self.album=album
+        
+    @classmethod
+    def create_table(self):
+        sql = """
+         CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+    #CURSOR.execute(sql)
+# CONN = sqlite3.connect('sqlite3/music.db')
+# CURSOR = CONN.cursor()
